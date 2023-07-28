657d5c299c981c3ba01bbe30fa99274a905b2af6
Karan <karan@noesis.tech>
2023-07-28 09:38:35 +0530

    order bug fixed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -9,10 +9,10 @@ const Message = ({ sender, text }) => {
   return (
     <div className="chat-message my-2">
       <div className={`flex items-end ${sender === 'user' ? 'justify-end' : ''}`}>
-        <div className={`flex flex-col space-y-2 text-sm max-w-xs mx-2 order-${sender === 'user' ? '1' : '2'} items-${sender === 'user' ? 'end' : 'start'}`}>
+        <div className={`flex flex-col space-y-2 text-sm max-w-xs mx-2 order-${sender === 'user' ? '1' : 'last'} items-${sender === 'user' ? 'end' : 'start'}`}>
             <span className={`px-4 py-2 rounded-lg inline-block ${roundedClass} ${bgColor} ${textColor}`}>{text}</span>
         </div>
-        {sender !== "user" && <img src="../../App icon.png" alt="My profile" className="w-6 h-6 rounded-full order-1" />}
+        {sender !== "user" && <img src="../../App icon.png" alt="My profile" className="w-6 h-6 rounded-full order-first" />}
       </div>
     </div>
   );
