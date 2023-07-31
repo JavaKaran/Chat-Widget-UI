a09d046d4a84fa6919e51f111ec8bb861274da60
Karan <karan@noesis.tech>
2023-07-31 20:23:05 +0530

    ui mobile bugs resolved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,14 +24,14 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="flex sm:items-center justify-between py-3 bg-[#ebebebde] border-b-2 px-6 border-gray-200">
+    <div className="flex sm:items-center justify-between py-2 sm:py-3 bg-[#ebebebde] border-b-2 px-6 border-gray-200">
       <div className="relative flex items-center space-x-4">
-        <img src="../../App icon.png" alt="Brainstormer Logo" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full" />
+        <img src="../../App icon.png" alt="Brainstormer Logo" className="w-8 sm:w-16 h-8 sm:h-16 rounded-full" />
         <div className="flex flex-col leading-tight">
-          <div className="text-2xl mt-1 flex items-center">
+          <div className="text-lg sm:text-2xl mt-1 flex items-center">
             <span className="text-gray-700 mr-3">{bot.name}</span>
           </div>
-          <span className="text-sm text-gray-600">{bot.description}</span>
+          <span className="text-xs sm:text-sm text-gray-600">{bot.description}</span>
         </div>
       </div>
     </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,11 +10,11 @@ const Input = ({ textAreaRef, handleKeyDown, handleMessageSend, setIsTyping, isT
     }
 
     return (
-        <div className="border-t-2 border-gray-200 p-6 sm:mb-0">
+        <div className="border-t-2 border-gray-200 p-3 sm:p-6 sm:mb-0">
             <div className="relative flex justify-around">
                 <textarea type="text" placeholder="Write your message!" className="w-[90%] focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 outline outline-1 outline-gray-400/50 pl-4 bg-transparent rounded-md py-3 resize-none" ref={textAreaRef} onKeyDown={handleKeyDown} rows={'1'} />
                 <div className="items-center flex">
-                    <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-[#2C2D98] hover:bg-[#5658d8] focus:outline-none" onClick={handleSendClick} disabled={isTyping}>
+                    <button type="button" className="flex items-center justify-center rounded-lg p-3 ml-2 sm:ml-0 sm:px-4 sm:py-3 transition duration-500 ease-in-out text-white bg-[#2C2D98] hover:bg-[#5658d8] focus:outline-none" onClick={handleSendClick} disabled={isTyping}>
                         <span className="font-bold sm:block hidden">Send</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                             <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
