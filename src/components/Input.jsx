ca226fb8be20210c21b53dd8c7b2926f1089897b
Karan <karan@noesis.tech>
2023-08-16 12:31:43 +0530

    button functioning modified

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Input = ({ textAreaRef, handleMessageSend, setIsTyping, isTyping }) => {
+const Input = ({ textAreaRef, handleMessageSend, setIsTyping, isTyping, disabled }) => {
     
     const textarea = textAreaRef.current;
 
@@ -14,7 +14,7 @@ const Input = ({ textAreaRef, handleMessageSend, setIsTyping, isTyping }) => {
     }
 
     const handleKeyDown = (e) => {
-        if (e.key === 'Enter' && !e.shiftKey) {
+        if (e.key === 'Enter' && !e.shiftKey && !disabled) {
             if (textAreaRef.current.value.trim() !== ''){
                 if (!isTyping) {
                     e.preventDefault();
@@ -53,7 +53,7 @@ const Input = ({ textAreaRef, handleMessageSend, setIsTyping, isTyping }) => {
                     onChange={handleTextareaChange}
                     />
                 <div className="items-center flex">
-                    <button type="button" className="flex items-center justify-center rounded-lg px-3 py-1 ml-2 sm:ml-0 sm:px-4 sm:py-3 transition duration-500 ease-in-out text-white bg-[#2C2D98] hover:bg-[#5658d8] focus:outline-none h-full max-h-[25px] sm:max-h-[50px] border-0" onClick={handleSendClick} disabled={isTyping}>
+                    <button type="button" className="flex items-center justify-center rounded-lg px-3 py-1 ml-2 sm:ml-0 sm:px-4 sm:py-3 transition duration-500 ease-in-out text-white bg-[#2C2D98] hover:bg-[#5658d8] focus:outline-none h-full max-h-[25px] sm:max-h-[50px] border-0" onClick={handleSendClick} disabled={isTyping || disabled}>
                         <span className="font-bold sm:block hidden sm:mr-2 mr-0">Send</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3 w-3 sm:h-6 sm:w-6 transform rotate-90">
                             <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
