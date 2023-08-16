07249d659e83686b9cdc1c7814cf3d54f4e85ad9
Karan <karan@noesis.tech>
2023-08-16 11:10:50 +0530

    ui bugs fixed

--- src/components/TypingIndicator.jsx
+++ src/components/TypingIndicator.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TypingIndicator = ({ image }) => {
     return (
         <div className='flex items-end my-1 sm:my-2'>
-            <img src={image} alt="Avatar" className="w-6 h-6 rounded-full" />
+            <img src={image} alt="Avatar" className="w-7 h-7 rounded-full" />
             <div className="flex items-end ml-2 bg-gray-300 rounded-bl-none p-2 sm:p-4 rounded-lg chat-typing-indicator">
                 <span className="dot"></span>
                 <span className="dot"></span>
