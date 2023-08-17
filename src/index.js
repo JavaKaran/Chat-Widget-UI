373e38a814ad5c12872aafdf3f846f48f1f8cc34
Karan <karan@noesis.tech>
2023-08-17 19:32:56 +0530

    iframe bug fixed

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const App = () => {
   const isInsideIframe = window !== parentWindow;
 
   const messageIncoming = (e) => {
+    console.log("e.data", e, e.data)
     console.log("condition 1", allowedDomains.includes(e.origin) )
     console.log("condition 2", e.origin.includes(e.data) )
 
