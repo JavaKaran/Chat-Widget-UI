ec508de71c917ef48d084b76d314f8b04fddf2dd
Karan <karan@noesis.tech>
2023-11-03 14:23:32 +0530

    cursor style modified

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,8 @@ import ConversationPDF from "./ConversationPDF";
 const Menu = ({ showHeaderMenu, messages, downloadPDF }) => {
     return (
         <div className={`p-2 bg-white transition ease-in-out rounded-md duration-500 z-30 shadow-md absolute right-[10px] ${showHeaderMenu ? 'show-menu' : 'hide-menu'}`}>
-            <p className="mb-2 text-[12px] leading-[12px]" onClick={downloadPDF}>Share entire conversation (Image)</p>
-            <PDFDownloadLink document={<ConversationPDF messages={messages} />} fileName="Chat.pdf" className="text-[12px] text-black no-underline">
+            <p className="mb-2 text-[12px] leading-[12px] cursor-pointer" onClick={downloadPDF}>Share entire conversation (Image)</p>
+            <PDFDownloadLink document={<ConversationPDF messages={messages} />} fileName="Chat.pdf" className="text-[12px] text-black no-underline cursor-pointer">
                 <p className="mb-2 text-[12px] leading-[12px]">Share entire conversation</p>
             </PDFDownloadLink>
             <p className="mb-1 text-[12px] leading-[12px]">Privacy Policy</p>
